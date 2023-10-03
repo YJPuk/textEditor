08d9dbcb181efc71bca13bffbeeef80d1751ae1b
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-10-03 18:00:58 +1100

    added event handler for beforeinstallprompt, butinstall and appinstall in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,23 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    window.delayedPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const installPrompt = window.delayedPrompt;
+    if(!installPrompt) {
+        console.log('App already installed!');
+        return;
+    }
+    installPrompt();
+    window.delayedPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.delayedPrompt = null;
+});
